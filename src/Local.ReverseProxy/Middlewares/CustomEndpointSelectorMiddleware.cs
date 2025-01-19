30925e70f2382b18dee335bb9dd981ebfa0b5aa9
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-01-19 20:19:59 +0000

    working with swagger rverproxy added swaggger sample to test

---
+++ src/Local.ReverseProxy/Middlewares/CustomEndpointSelectorMiddleware.cs
@@ -0,0 +1,44 @@
+﻿namespace Local.ReverseProxy.Middlewares
+{
+    /// <summary>
+    /// this sample is for swagger through reverse proxy
+    /// </summary>
+    public class CustomEndpointSelectorMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly EndpointDataSource _endpointDataSource;
+
+        public CustomEndpointSelectorMiddleware(RequestDelegate next, EndpointDataSource endpointDataSource)
+        {
+            _next = next;
+            _endpointDataSource = endpointDataSource;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var endpoint = context.GetEndpoint();
+            if ((endpoint == null || endpoint.DisplayName == "AllRoute") 
+                && !string.IsNullOrEmpty(context.Request.Headers["Referer"].ToString()) )
+            {
+                var endpoints = _endpointDataSource.Endpoints;
+                var refererUrlSegments = context.Request.Headers["Referer"].ToString().Split('/');
+                // If the referer URL has more than 3 segments, then it is a swagger URL
+                if (refererUrlSegments.Length > 3)
+                {
+                    // Select the matching first endpoint from the list
+                    //write your custom logic here
+                    var pathPrefix = refererUrlSegments[3] ?? string.Empty;
+                    var selectedEndpoint = endpoints
+                        .FirstOrDefault(e => e.DisplayName == pathPrefix 
+                                            || (e.Metadata.Count > 1 && e.Metadata[1].ToString().Contains($"/{pathPrefix}/") ));
+                    if (selectedEndpoint != null)
+                    {
+                        context.SetEndpoint(selectedEndpoint);
+                    }
+                }                
+            }
+
+            await _next(context);
+        }
+    }
+}
--- src/Local.ReverseProxy/Program.cs
+++ src/Local.ReverseProxy/Program.cs
@@ -3,7 +3,7 @@ using Local.ReverseProxy.Transforms;
 using Microsoft.AspNetCore.Routing.Matching;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<EndpointSelector, CustomEndpointSelector>();
+//builder.Services.AddSingleton<EndpointSelector, CustomEndpointSelector>();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
      .AddTransforms(builderContext =>
@@ -15,6 +15,7 @@ builder.Services.AddReverseProxy()
 
 var app = builder.Build();
 
+app.UseMiddleware<CustomEndpointSelectorMiddleware>();
 app.Use(async (context, next) =>
 {
     var endpoint = context.GetEndpoint();
