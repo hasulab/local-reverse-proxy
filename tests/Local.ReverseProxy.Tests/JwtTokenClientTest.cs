4f8ba5ef687bd42a14b717bad519c5a61931237d
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-02-16 22:38:03 +0000

    added sample place holder for jwttoken client

---
+++ tests/Local.ReverseProxy.Tests/JwtTokenClientTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Identity.Web;
+
+namespace Local.ReverseProxy.Tests
+{
+    public class JwtTokenClientTest : TestBase
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
